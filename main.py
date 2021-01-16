f8346aed0a4c4c1d9bc65aa0ffa261a93e66ca65
Máté Kovács <bende@Mates-Air.fritz.box>
2021-01-16 18:03:59 +0100

    finally changed main code to the new refactored one

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-import sys
 import time
+import sys
 from random import choice
-#import pyaudio
 import sounddevice as sd
-from scipy.io.wavfile import write
 import numpy as np
-from aubio import notes, pitch
+from aubio import pitch
+from rich.live import Live
+from rich.console import RenderGroup
+from rich.panel import Panel
+from rich.text import Text
+from rich.prompt import Prompt
+from rich.console import Console
+from rich import box, print
 from helpers import pitch2note, NOTES_PER_STRING
+from objects import SCALES, NOTE_NAMES, Scale
 
-CHUNK = 1024
-FORMAT = pyaudio.paFloat32
-CHANNELS = 1
+# Set up constants for audio input & pitch detection
 RATE = 44100
-RECORD_SECONDS = 5000
+TOLERANCE = 0.8
+DOWNSAMPLE = 1
+WIN_S = 2048 // DOWNSAMPLE # fft size
+HOP_S = 1024  // DOWNSAMPLE # hop size
 
-'''TODO:
+pitch_o = pitch("yinfft", WIN_S, HOP_S, RATE)
+pitch_o.set_unit("Hz")
+pitch_o.set_silence(-40)
+pitch_o.set_tolerance(TOLERANCE)
+
+FRAME_SIZE = 1024  # How many samples per frame?
+FRAMES_PER_DETECT = 10 # How many frames per note detection?
+
+console = Console() # defining Rich console
+
+'''TODO game modes ideas::
 - Only whole notes or only half notes or both
 - Only 1 string or multiple strings
 - implement the groups from here: https://medium.com/@aslushnikov/memorizing-fretboard-a9f4f28dbf03
@@ -22,97 +39,163 @@ RECORD_SECONDS = 5000
 - Select notes from multiple string (and maybe identify what is the string that is playing)
 '''
 
-# Pitch
-tolerance = 0.8
-downsample = 1
-win_s = 4096 // downsample # fft size
-hop_s = 1024  // downsample # hop size
-#notes_o = notes("default", win_s, hop_s, RATE)
-pitch_o = pitch("yinfft", win_s, hop_s, RATE)
-pitch_o.set_unit("Hz")
-pitch_o.set_silence(-40)
-pitch_o.set_tolerance(tolerance)
+def main():
 
-while True:
+    console.clear()
+    text = Text('\nFRETBOARD LEARNER\n(press cmd+C to quit at any time)\n', justify='center')
+    text.stylize('bold blue', 0, 18)
+    text.stylize('grey37', 18)
+    console.print(Panel(text), justify='center')
+
+    try:
+
+        available_game_modes = {
+            'Play random notes on a given string': mode_1_random_notes,
+            'Play random notes in a given scale': mode_2_scale_notes
+        }
+
+        game_mode = choose(list(available_game_modes.keys()), choice_name='game modes')
+
+        # play selected game mode
+        available_game_modes[game_mode]()
+
+    except KeyboardInterrupt:
+        console.clear()
+        exit_text = Text('\nThanks for playing! :)\n', justify='center')
+        exit_text.stylize('bold blue')
+        console.print(Panel(exit_text))
+        sys.exit()
+
+def mode_2_scale_notes():
+
+    console.print('\nYou need to select a key and a scale type')
+
+    scale_key = choose(NOTE_NAMES, choice_name='keys')
+    scale_type = choose(list(SCALES.keys()), choice_name='scales')
+
+    scale = Scale(scale_type, scale_key, add_root_as_last=False)
+    notes = scale.notes
+
+    play_mode(notes)
+
+
+def mode_1_random_notes():
     
-    inp = input('Which string?\n')
+    strings = list(NOTES_PER_STRING.keys())
+    string = choose(strings, choice_name='strings')
+    notes = NOTES_PER_STRING[string]
+    notes = [note[:-1] for note in notes]
 
-    if inp not in('EADGBe'):
-        print('Need to select from EADGBe')
-        continue
+    play_mode(notes)
+
+def play_mode(notes):
+
+    console.clear()
+
+    score = 0
+    prev_note_choice = None
+
+    with Live(refresh_per_second=20) as live:
+        note_choice = ''
+        note_played = ''
+        note_color = 'red'
+
+        while True:
+            note_choice = choice(notes)
+            live.update(generate_panel(notes, note_choice, note_played, note_color, score))
+            if prev_note_choice == note_choice:
+                continue
+            else:
+                prev_note_choice = note_choice
+
+            while True:
+                note_played = detect_note()
+                is_right_note = note_played == note_choice
+                note_color = 'green' if is_right_note else 'red'
+                live.update(generate_panel(notes, note_choice, note_played, note_color, score))
+                if is_right_note:
+                    note_choice = '[green bold]Good job![/]'
+                    score += 1
+                    live.update(generate_panel(notes, note_choice, note_played, note_color, score))
+                    time.sleep(1)
+                    break
+
+def generate_panel(notes, note_choice, note_played, color, score):
+
+    if note_choice not in notes:
+        # it means note_choice is 'Good job!' when the player hits the note
+        play_note_row = note_choice
     else:
-        break
+        play_note_row = f'Play this note: [blue bold]{note_choice}[/]'
+    note_played_row = f'You played: [{color} bold]{note_played}[/]'
 
-prev_note_choice = None
+    all_notes_row = Text.assemble(
+        ('\nAll available notes: ', 'grey53 bold'),
+        ((', ').join(notes), 'grey53')
+    )
 
-print('\n\nPress cmd+c to quit at any time\n\n')
+    current_score_text = Text(f'\nCurrent score: {score}\n')
+    current_score_text.stylize('yellow bold', 16)
 
-while True:
+    quit_text = Text('\n\n(press cmd+C to quit at any time)')
+    quit_text.stylize('grey37')
 
-    note_choice = choice(NOTES_PER_STRING[inp])
-    if prev_note_choice == note_choice:
-        continue
-    else:
-        prev_note_choice = note_choice
+    panel_group = RenderGroup(
+        current_score_text,
+        Panel(play_note_row),
+        Panel(note_played_row),
+        all_notes_row,
+        quit_text
+    )
 
-    note_choice_without_number = note_choice[:-1]
-    print(f'Play this note: {note_choice_without_number}')
+    return Panel(panel_group, title='FRETBOARD LEARNER', box=box.ASCII)
 
-    p = pyaudio.PyAudio()
+   
+def detect_note():
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    #print("* recording")
-
-    frames = []
+    stream = sd.Stream(channels=1, blocksize=1024)
+    stream.start()
     notes_list = []
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        buffer = stream.read(CHUNK)
-        frames.append(buffer)
+    while True:
+        buf = np.frombuffer(stream.read(FRAME_SIZE)[0], np.float32)
+        pitch_of_sample = pitch_o(buf)[0]
+        if pitch_of_sample:
+            note_played = pitch2note(pitch_of_sample)
+            notes_list.append(note_played[:-1])
 
-        signal = np.frombuffer(buffer, dtype=np.float32)
-
-        #new_note = notes_o(signal) # we get note from pitch as it is more accurate
-        pitch = pitch_o(signal)[0]
-        #confidence = pitch_o.get_confidence()
-        if pitch != 0:
-            note_played = pitch2note(pitch)
-            notes_list.append(note_played[:-1]) # we append only note and not number (eg. E and not E2)
-
-            if len(notes_list) == 10:
-                # We listen for 10 signals and then select the most frequent note in the list
-                # This is because when you pluck a note the frequency can deviate as you pluck it strongly
+            if len(notes_list) == FRAMES_PER_DETECT:
                 most_likely_note = max(notes_list, key=notes_list.count)
-                sys.stdout.write(f'\rYou played: {most_likely_note}  ')
-                sys.stdout.flush()
-                if most_likely_note == note_choice_without_number:
-                    # we hit the desired note
-                    print('Good job!')
-                    break
-                else:
-                    # if we don't hit the desired note, we get another 10 signals
-                    notes_list = []
-    
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    time.sleep(1) # so the user is not bombarded by constant new things
-
-    '''
-        if (new_note[0] != 0):
-            note_str = ' '.join(["%.2f" % i for i in new_note])
-            print(f'{new_note[0]} - {pitch2note(int(new_note[0]))}')
-            #print(pitch_o(signal)
-    '''
-
-#print("* done recording")
+                stream.stop()
+                stream.close()
+                return most_likely_note
 
 
+def choose(choices, choice_name='choices'):
 
+    console.clear()
 
+    choices_str = Text(f'Please select from the available {choice_name}:\n\n', 'bold blue')
+
+    for i, choice in enumerate(choices):
+
+        num = Text(str(i + 1), 'green')
+        choices_str.append(Text('  [', 'white'))
+        choices_str.append(num)
+        choices_str.append(Text(']', 'white'))
+        choices_str.append(Text(f'\t{choice}\n', 'white'))
+
+    print(Panel(choices_str))
+
+    inp = Prompt.ask(
+        f'Choose from the {choice_name} (enter one of the numbers)',
+        choices=[str(i) for i in range(1, len(choices) + 1)],
+        default=1,
+        show_choices=False,
+        show_default=False
+    )
+
+    return choices[int(inp) - 1]
+
+if __name__ == "__main__":
+    main()
---
+++ old_code.py
@@ -0,0 +1,114 @@
+import sys
+import time
+from random import choice
+#import pyaudio
+import sounddevice as sd
+from scipy.io.wavfile import write
+import numpy as np
+from aubio import notes, pitch
+from helpers import pitch2note, NOTES_PER_STRING
+
+CHUNK = 1024
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 5000
+
+"""
+THIS IS THE OLD VERSION, KEEPING IT JUST COZ IM ATTACHED BUT main.py IS THE WORKING ONE
+"""
+
+# Pitch
+tolerance = 0.8
+downsample = 1
+win_s = 4096 // downsample # fft size
+hop_s = 1024  // downsample # hop size
+#notes_o = notes("default", win_s, hop_s, RATE)
+pitch_o = pitch("yinfft", win_s, hop_s, RATE)
+pitch_o.set_unit("Hz")
+pitch_o.set_silence(-40)
+pitch_o.set_tolerance(tolerance)
+
+while True:
+    
+    inp = input('Which string?\n')
+
+    if inp not in('EADGBe'):
+        print('Need to select from EADGBe')
+        continue
+    else:
+        break
+
+prev_note_choice = None
+
+print('\n\nPress cmd+c to quit at any time\n\n')
+
+while True:
+
+    note_choice = choice(NOTES_PER_STRING[inp])
+    if prev_note_choice == note_choice:
+        continue
+    else:
+        prev_note_choice = note_choice
+
+    note_choice_without_number = note_choice[:-1]
+    print(f'Play this note: {note_choice_without_number}')
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    #print("* recording")
+
+    frames = []
+    notes_list = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        buffer = stream.read(CHUNK)
+        frames.append(buffer)
+
+        signal = np.frombuffer(buffer, dtype=np.float32)
+
+        #new_note = notes_o(signal) # we get note from pitch as it is more accurate
+        pitch = pitch_o(signal)[0]
+        #confidence = pitch_o.get_confidence()
+        if pitch != 0:
+            note_played = pitch2note(pitch)
+            notes_list.append(note_played[:-1]) # we append only note and not number (eg. E and not E2)
+
+            if len(notes_list) == 10:
+                # We listen for 10 signals and then select the most frequent note in the list
+                # This is because when you pluck a note the frequency can deviate as you pluck it strongly
+                most_likely_note = max(notes_list, key=notes_list.count)
+                sys.stdout.write(f'\rYou played: {most_likely_note}  ')
+                sys.stdout.flush()
+                if most_likely_note == note_choice_without_number:
+                    # we hit the desired note
+                    print('Good job!')
+                    break
+                else:
+                    # if we don't hit the desired note, we get another 10 signals
+                    notes_list = []
+    
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    time.sleep(1) # so the user is not bombarded by constant new things
+
+    '''
+        if (new_note[0] != 0):
+            note_str = ' '.join(["%.2f" % i for i in new_note])
+            print(f'{new_note[0]} - {pitch2note(int(new_note[0]))}')
+            #print(pitch_o(signal)
+    '''
+
+#print("* done recording")
+
+
+
+
